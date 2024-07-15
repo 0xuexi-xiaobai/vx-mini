a119aab2139a467e320ca1b6eb433058f1d8f69d
junyao song <songjunyao@psyai.net>
2024-07-15 09:34:53 +0800

    prompt ready

--- react-admin-frontend/src/views/prompt/index.jsx
+++ react-admin-frontend/src/views/prompt/index.jsx
@@ -1,11 +1,25 @@
-import React, { useState } from 'react';
-import { Card, Button, Upload, Input, message } from 'antd';
+import { Card, Button, Upload, Input, message, List, Avatar, Skeleton } from 'antd';
 import { uploadPrompt } from '../../api/prompt';
+import React, { useEffect, useState } from 'react';
+const count = 3;
+const fakeDataUrl = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat,picture&noinfo`;
 
 const ImageUploader = () => {
   const [imageUrl, setImageUrl] = useState('');
   const [textValue, setTextValue] = useState('');
-
+  const [initLoading, setInitLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    fetch(fakeDataUrl)
+      .then((res) => res.json())
+      .then((res) => {
+        setInitLoading(false);
+        setData(res.results);
+        setList(res.results);
+      });
+  }, []);
   const handleImageChange = (info) => {
     const { status } = info.file;
     if (status === 'done') {
@@ -71,6 +85,43 @@ const ImageUploader = () => {
       <div className="ant-upload-text">点击上传图片</div>
     </div>
   );
+  const onLoadMore = () => {
+    setLoading(true);
+    setList(
+      data.concat(
+        [...new Array(count)].map(() => ({
+          loading: true,
+          name: {},
+          picture: {},
+        })),
+      ),
+    );
+    fetch(fakeDataUrl)
+      .then((res) => res.json())
+      .then((res) => {
+        const newData = data.concat(res.results);
+        setData(newData);
+        setList(newData);
+        setLoading(false);
+        // Resetting window's offsetTop so as to display react-virtualized demo underfloor.
+        // In real scene, you can using public method of react-virtualized:
+        // https://stackoverflow.com/questions/46700726/how-to-use-public-method-updateposition-of-react-virtualized
+        window.dispatchEvent(new Event('resize'));
+      });
+  };
+  const loadMore =
+    !initLoading && !loading ? (
+      <div
+        style={{
+          textAlign: 'center',
+          marginTop: 12,
+          height: 32,
+          lineHeight: '32px',
+        }}
+      >
+        <Button onClick={onLoadMore}>loading more</Button>
+      </div>
+    ) : null;
 
   return (
     <div className="app-container">
@@ -98,8 +149,31 @@ const ImageUploader = () => {
         <Button type="primary" style={{ marginTop: '10px' }} onClick={handleUploadClick}>
           上传到服务器
         </Button>
+        
       </Card>
+      <List
+        className="demo-loadmore-list"
+        loading={initLoading}
+        itemLayout="horizontal"
+        loadMore={loadMore}
+        dataSource={list}
+        renderItem={(item) => (
+          <List.Item
+            actions={[<a key="list-loadmore-edit">edit</a>, <a key="list-loadmore-more">more</a>]}
+          >
+            <Skeleton avatar title={false} loading={item.loading} active>
+              <List.Item.Meta
+                avatar={<Avatar src={item.picture.large} />}
+                title={<a href="https://ant.design">{item.name?.last}</a>}
+                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+              />
+              <div>content</div>
+            </Skeleton>
+          </List.Item>
+        )}
+      />
     </div>
+
   );
 };
 
